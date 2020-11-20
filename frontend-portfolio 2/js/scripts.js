0731e0249dd9c3f841619be29b530c6b63aaa22e
neetisharma <36397406+neetisharma@users.noreply.github.com>
2020-11-19 23:05:30 -0800

    added javascript

--- frontend-portfolio 2/js/scripts.js
+++ frontend-portfolio 2/js/scripts.js
@@ -1,1 +1,11 @@
 console.log("Welcome to my portfolio site")
+
+function menuToggle() {
+    console.log("clicked")
+    var x = document.getElementById('myNavtoggle');
+  if (x.className === 'navtoggle') {
+    x.className += ' responsive';
+  } else {
+    x.className = 'navtoggle';
+  }
+}
